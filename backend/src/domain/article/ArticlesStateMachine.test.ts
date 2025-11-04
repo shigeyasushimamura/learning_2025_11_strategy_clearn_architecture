2f62236eb664bceac6a59e7f1dff1e052e3adf08
shigeyasushimamura <sgys.mtk@gmail.com>
2025-11-04 20:44:42 +0900

    feature: construct test environment

---
+++ backend/src/domain/article/ArticlesStateMachine.test.ts
@@ -0,0 +1,4 @@
+import { ArticleStateMachine } from "./ArticleStateMachine";
+import { ArticleState } from "./ArticleState";
+import { ArticleEvent } from "./ArticleEvent";
+import { describe, expect } from "vitest";
